51a678a1bd21fc2366467fdb7d2df4f5cdf41e5a
Rajan Sahu <55847984+rajansahu713@users.noreply.github.com>
2021-04-28 18:01:13 +0530

    Add files via upload

---
+++ student/api/views.py
@@ -0,0 +1,11 @@
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework import viewsets
+# Create your views here.
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
